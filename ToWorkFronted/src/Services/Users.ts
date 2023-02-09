288f07e2974970345eb47f1f7c0c5df0001f646b
elmm <elmm.programing@gmail.com>
2023-02-09 15:21:25 -0400

    User services created

---
+++ ToWorkFronted/src/Services/Users.ts
@@ -0,0 +1,14 @@
+import { User } from '../Types/common'
+import { RestApiClient } from '../Utils/RestApiClient'
+
+export const getUsers = async()=>{
+return RestApiClient("users/")
+}
+export const loginUser = async (user:User)=>{
+return RestApiClient("users/users/login",{
+  method: 'POST',
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify(user),
+})
+
+}
