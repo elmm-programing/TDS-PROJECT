db3d552cef287cc8fcda40b84bc6dc9a4ead15ff
elmm <elmm.programing@gmail.com>
2023-02-10 14:51:13 -0400

    Repetitive code refactor in login and register and eslint and prettier added

--- ToWorkFronted/src/Pages/Login.tsx
+++ ToWorkFronted/src/Pages/Login.tsx
@@ -1,46 +1,81 @@
-import { BaseSyntheticEvent, SyntheticEvent, useState } from 'react'
+import { BaseSyntheticEvent, useState } from 'react'
 import { loginUser } from "../Services/Users"
 import { User } from '../Types/common'
 import { useNavigate } from "react-router-dom";
-function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-const navigate = useNavigate();
-  const login = async (e: BaseSyntheticEvent) => {
-    e.preventDefault()
-    let user: User = {
-      name:"", 
-      lastName: "",
-      email: email,
-      username: email,
-      password: password,
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Fade from 'react-bootstrap/esm/Fade';
 
+
+function Login() {
+  const [user, setUser] = useState({ email: '', password: '' })
+  const [isLoading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const [alertText, setAlertText] = useState("");
+  const [variant, setVariant] = useState("")
+  const navigate = useNavigate();
+
+  const onChangeInput = (e: BaseSyntheticEvent) => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+  }
+
+  const login = async (e: BaseSyntheticEvent) => {
+    setLoading(true)
+    e.preventDefault()
+    let newUser: User = {
+      name: "",
+      lastName: "",
+      email: user.email,
+      username: user.email,
+      password: user.password,
     }
-    let response: boolean | string =await loginUser(user);
+    let response: boolean | string = await loginUser(newUser);
     if (response == true) {
+      setLoading(false);
+      setVariant("success")
+      setAlertText("The user and password are correct!")
+      setAlert(true)
       navigate("/");
-    }else if(response == "User Not Found"){
-      alert("User Not Found")
-    }else{
+    } else if (response == "User Not Found") {
+      setLoading(false);
+      setVariant("danger")
+      setAlertText("The user was not found")
+      setAlert(true)
+    } else {
+      setLoading(false);
+      setVariant("danger")
+      setAlertText("The Password is incorrect")
+      setAlert(true)
       console.log(false)
     }
   }
 
   return (
-    <form className='form-signin w-100 m-auto text-center' onSubmit={login}>
-      <h1 className="h3 mb-3 fw-normal">Sign in</h1>
+    <>
+      <form className='form-signin w-100 m-auto text-center' onSubmit={login}>
+        <h1 className="h3 mb-3 fw-normal">Sign in</h1>
+        {Object.entries(user).map(([key]) => (
+          <div className="form-floating mb-3" key={key}>
+            <input type="text" name={key} required className="form-control" onChange={onChangeInput} id="floatingInput" placeholder="Email or Username" />
+            <label >{key == 'email' ? "Email or Username" : key.charAt(0).toUpperCase() + key.slice(1)}</label>
+          </div>
 
-      <div className="form-floating mb-3">
-        <input type="text" className="form-control" onChange={e => setEmail(e.target.value)} id="floatingInput" placeholder="Email or Username" />
-        <label >Email or Username</label>
-      </div>
-      <div className="form-floating mb-3">
-        <input type="password" className="form-control" onChange={e => setPassword(e.target.value)} id="floatingPassword" placeholder="Password" />
-        <label >Password</label>
-      </div>
+        ))}
 
-      <button className="w-100 btn btn-lg btn-primary" type="submit" >Sign in</button>
-    </form>
+        <Button variant="primary"
+          disabled={isLoading}
+          className="w-100 btn-lg" type="submit">{isLoading ? 'Loading…' : 'Sign in'}</Button>
+      </form>
+      <div style={{ minHeight: '150px' }}>
+        <Fade in={alert} >
+          <Alert show={alert} className='position-absolute bottom-0 end-0 w-auto' variant={variant} onClose={() => setAlert(false)} dismissible >
+            {alertText}
+          </Alert>
+
+        </Fade >
+      </div>
+    </>
   )
 }
 
--- ToWorkFronted/src/Pages/Register.tsx
+++ ToWorkFronted/src/Pages/Register.tsx
@@ -1,31 +1,28 @@
-import { BaseSyntheticEvent, SyntheticEvent, useState } from 'react'
+import { BaseSyntheticEvent, useState } from 'react'
 import { addUser } from "../Services/Users"
-import { User } from '../Types/common'
 import { useNavigate } from "react-router-dom";
 
 function Register() {
-
-  const [name, setName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-const navigate = useNavigate();
+  const [user, setUser] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: '',
+  })
+  const onChangeInput = (e: BaseSyntheticEvent) => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+  }
+  const navigate = useNavigate();
   const register = async (e: BaseSyntheticEvent) => {
     e.preventDefault()
-    let user: User = {
-      name:name, 
-      lastName: lastName,
-      email: email,
-      username: username,
-      password: password,
-    }
-    let response: boolean | string =await addUser(user);
+    let response: boolean | string = await addUser(user);
     if (response == true) {
       navigate("/");
-    }else if(response == "El Usuario ya existe"){
+    } else if (response == "El Usuario ya existe") {
       alert("El Usuario ya existe")
-    }else{
+    } else {
       console.log(false)
     }
   }
@@ -33,24 +30,14 @@ const navigate = useNavigate();
   return (
     <form className='form-signin w-100 m-auto text-center' onSubmit={register}>
       <h1 className="h3 mb-3 fw-normal">Sign out</h1>
-<div className="form-floating mb-3">
-        <input type="text" className="form-control" onChange={e => setName(e.target.value)}  placeholder="name@example.com" />
-        <label >Name</label>
-      </div><div className="form-floating mb-3">
-        <input type="text" className="form-control" onChange={e => setLastName(e.target.value)}  placeholder="name@example.com" />
-        <label >LastName</label>
-      </div><div className="form-floating mb-3">
-        <input type="email" className="form-control" onChange={e => setEmail(e.target.value)}  placeholder="name@example.com" />
-        <label >Email address</label>
-      </div>
-      <div className="form-floating mb-3">
-        <input type="text" className="form-control" onChange={e => setUsername(e.target.value)}  placeholder="name@example.com" />
-        <label >Username</label>
-      </div>
-      <div className="form-floating mb-3">
-        <input type="password" className="form-control" onChange={e => setPassword(e.target.value)}  placeholder="Password" />
-        <label >Password</label>
-      </div>
+      {Object.entries(user).map(([key]) => (
+        <div className="form-floating mb-3" key={key}>
+          <input type={key == "email" || key == "password" ? key : "text"} name={key} required className="form-control" onChange={onChangeInput}  placeholder="Email or Username" />
+          <label >{key.charAt(0).toUpperCase() + key.slice(1)}</label>
+        </div>
+
+      ))}
+
       <button className="w-100 btn btn-lg btn-primary" type="submit" >Sign out</button>
     </form>
   )
