fe677803b107de2ec858c0542f69c9fdf1904a6c
elmm-programing <86208465+elmm-programing@users.noreply.github.com>
2023-04-07 14:16:40 -0400

    Autocomplete format

--- ToWorkFronted/src/Components/AutoComplete.tsx
+++ ToWorkFronted/src/Components/AutoComplete.tsx
@@ -2,40 +2,41 @@ import { useState } from "react";
 import Form from "react-bootstrap/esm/Form";
 import { Highlighter, Typeahead } from 'react-bootstrap-typeahead'; // ES2015
 export function AutoComplete() {
-   const [singleSelections, setSingleSelections] = useState([]);
+  const [singleSelections, setSingleSelections] = useState([]);
   const [multiSelections, setMultiSelections] = useState([]);
-   const props = {};
-var options = [
-  'John',
-  'Miles',
-  'Charles',
-  'Herbie',
-];
- props.renderMenuItemChildren = (option, { text }) => (
-        <>
-          <Highlighter search={text}>{option}</Highlighter>,
-          <div>
-            <small>Population: {option}</small>
-          </div>
-        </>
-      );
+  const props = {};
+  var options = [
+    'John',
+    'Miles',
+    'Charles',
+    'Herbie',
+  ];
+  props.renderMenuItemChildren = (option, { text }) => (
+    <>
+      <Highlighter search={text}>{option}</Highlighter>,
+      <div>
+        <small>Population: {option}</small>
+      </div>
+    </>
+  );
   return (
     <>
       <Form.Group>
         <Typeahead
-         {...props}
+          {...props}
           id="basic-typeahead-single"
           labelKey="name"
           options={options}
           inputProps={{
-    className: 'my-custom-classname',
-    style: {
-      'width': 30+'rem',
-    }
-  }}
+            className: 'my-custom-classname',
+            style: {
+              'width': 30 + 'rem',
+            }
+          }}
           placeholder="Choose a state..."
           selected={singleSelections}
         />
       </Form.Group>
-          </>
-  );}
+    </>
+  );
+}
